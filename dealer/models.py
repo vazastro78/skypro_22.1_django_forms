4381086add4e7760f577d6498ee29ed966e2270f
Valeev Azamat <azamat@mail.sao.ru>
2024-08-15 17:02:34 +0300

    Задание 3. Product и  Category связь между таблицами «Один ко многим»

--- dealer/models.py
+++ dealer/models.py
@@ -1,20 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
     def __str__(self):
         # Строковое отображение объекта
-        return f'{self.__class__.__name__}'
+        return f'{self.category_name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория' # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории' # Настройка для наименования набора объектов
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=120, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    preview_image = models.ImageField(upload_to="preview/", verbose_name="изображение", null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.FloatField(verbose_name='цена')
+    created_at = models.DateTimeField(verbose_name='дата создания')
+    updated_at = models.DateTimeField(verbose_name='дата изменения')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.product_name} {self.description} {self.preview_image} {self.category} {self.price} {self.created_at} {self.updated_at}'
 
     class Meta:
         verbose_name = 'продукт' # Настройка для наименования одного объекта
         verbose_name_plural = 'продукты' # Настройка для наименования набора объектов
 
-class Category(models.Model):
-    def __str__(self):
-        # Строковое отображение объекта
-        return f'{self.__class__.__name__}'
-
-    class Meta:
-        verbose_name = 'категория' # Настройка для наименования одного объекта
-        verbose_name_plural = 'категории' # Настройка для наименования набора объектов
