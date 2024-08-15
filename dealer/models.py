9fe87f24058812d6a29c46cef32de2fb0914c5c4
Valeev Azamat <azamat@mail.sao.ru>
2024-08-15 16:29:04 +0300

    Задание2. В приложении каталога создайте модели: Product, Category. Опишите для них начальные настройки: __str__  и class Meta

--- dealer/models.py
+++ dealer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.__class__.__name__}'
+
+    class Meta:
+        verbose_name = 'продукт' # Настройка для наименования одного объекта
+        verbose_name_plural = 'продукты' # Настройка для наименования набора объектов
+
+class Category(models.Model):
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.__class__.__name__}'
+
+    class Meta:
+        verbose_name = 'категория' # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории' # Настройка для наименования набора объектов
