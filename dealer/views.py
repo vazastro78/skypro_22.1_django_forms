bcaa995df6ad77fda88608f068c6fdd04080bdf1
Valeev Azamat <azamat@mail.sao.ru>
2024-08-15 16:17:17 +0300

    django DEALER application created

---
+++ dealer/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        print( f'{username} ({email},{message})')
+    return render(request, 'mainapp/index.html')
