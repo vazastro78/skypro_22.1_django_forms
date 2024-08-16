f3ea10568bf595161f355242818117561a0e420e
Valeev Azamat <azamat@mail.sao.ru>
2024-08-16 20:34:49 +0300

    Шаблоны20.2 Задание2 список товаров на главной странице в цикле

--- dealer/views.py
+++ dealer/views.py
@@ -1,12 +1,23 @@
+from itertools import product
+
 from django.shortcuts import render
+
+from dealer.models import Product
+
 
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
-        print( f'{username} ({email},{message})')
-    return render(request, 'dealer/mainpage.html')
+    products = Product.objects.all()
+    for product_item in products:
+        max_lenght = 100
+        if len(product_item.description)>max_lenght:
+            product_item.description = product_item.description[0:max_lenght] + "..."
+
+
+    context = {
+        'product_list': products
+    }
+
+    return render(request, 'dealer/mainpage.html', context)
 
